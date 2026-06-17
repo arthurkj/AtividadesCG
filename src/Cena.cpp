eaec2dd9fe780b56ba2233efc7f13b1cda939489
Arthur Kist Juchem <arthur.juchem@olxbr.com>
2026-06-17 19:42:36 -0300

    Ajuste

--- src/Cena.cpp
+++ src/Cena.cpp
@@ -4,11 +4,15 @@
 #include <iostream>
 #include <vector>
 
+// Avisa o compilador que essa função existe em outro arquivo do projeto
 extern GLuint loadTexture(std::string filePath);
 
 // Implementação do loadSimpleOBJ
 int loadSimpleOBJ(std::string filePath, int &nVertices, std::string &texturePath, glm::vec3 &ka, glm::vec3 &kd, glm::vec3 &ks)
 {
+    ka = glm::vec3(0.1f);
+    kd = glm::vec3(0.7f);
+    ks = glm::vec3(0.5f);
     std::vector<glm::vec3> vertices;
     std::vector<glm::vec2> texCoords;
     std::vector<glm::vec3> normals;
@@ -32,7 +36,6 @@ int loadSimpleOBJ(std::string filePath, int &nVertices, std::string &texturePath
         {
             std::string mtlFileName;
             ssline >> mtlFileName;
-            
             std::string basePath = filePath.substr(0, filePath.find_last_of('/') + 1);
             
             std::ifstream arqMTL(basePath + mtlFileName);
@@ -122,22 +125,26 @@ int loadSimpleOBJ(std::string filePath, int &nVertices, std::string &texturePath
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
     
-
+    // Layout 0: Posição (3 floats)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
 
+    // Layout 1: Cor (3 floats)
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
 
+    // Layout 2: Normal (3 floats)
     glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
     glEnableVertexAttribArray(2);
     
+    // Layout 3: Textura (2 floats)
     glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, 11 * sizeof(GLfloat), (GLvoid*)(9 * sizeof(GLfloat)));
     glEnableVertexAttribArray(3);
     
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
+    // Dividimos por 11 pois agora cada vértice tem exatamente 11 valores
     nVertices = vBuffer.size() / 11;
 
     return VAO;
@@ -186,7 +193,7 @@ void loadScene(std::string filePath, Camera &camera, std::vector<Object3D> &obje
             obj.ka = ka; 
             obj.kd = kd; 
             obj.ks = ks;
-            obj.tParam = 0.0f;
+            obj.tParam = 0.0f; // Inicializa o controle de Bézier
             
             objects.push_back(obj);
         } else if (type == "PATH") {
